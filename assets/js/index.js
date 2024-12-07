5f73e4fbba60d7456e6c4550934b77a631053b01
Zahid Shaikh <zahid.786shaikh@gmail.com>
2024-12-07 16:09:28 +0530

    feat: completed receipie page challange

---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        rose: {
+          800: "#7a284e",
+          50: "#fff7fb",
+        },
+        stone: {
+          900: "#312e2c",
+          600: "#5f564d",
+          150: "#e3ddd7",
+          100: "#f3e5d7",
+        },
+        brown: {
+          800: "#854632",
+        },
+        white: "#ffffff",
+      },
+      spacing: {
+        100: "0.5rem",
+        150: "0.75rem",
+        200: "1rem",
+        300: "1.5rem",
+        400: "2rem",
+        500: "2.5rem",
+        600: "3rem",
+        1600: "8rem",
+      },
+      borderRadius: {
+        100: "0.5rem",
+        150: "0.75rem",
+        200: "1rem",
+        300: "1.5rem",
+        400: "2rem",
+        500: "2.5rem",
+        600: "3rem",
+        1600: "8rem",
+      },
+      "font-size": {
+        desktop: {
+          "text.preset.1": 40,
+          "text.preset.2": 28,
+          "text.preset.3": 20,
+          "text.preset.4": 16,
+        },
+        mobile: {
+          "text.preset.1": 36,
+        },
+      },
+      fontSize: {
+        16: "1rem",
+        20: "1.25rem",
+        28: "1.75rem",
+        36: "2.25rem",
+        40: "2.5rem",
+      },
+    },
+  },
+};
